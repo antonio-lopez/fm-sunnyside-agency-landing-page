29c158960c795fb1405700d1ca113f755a05031f
Antonio Lopez <pezlop202@gmail.com>
2022-04-30 18:01:58 -0700

    mobile menu

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', (e) => {
+  e.preventDefault();
+  menu.classList.toggle('hidden');
+  menu.classList.toggle('flex');
+});
+
+//remove hidden
+// add flex
